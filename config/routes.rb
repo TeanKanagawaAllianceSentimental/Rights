a33665f9e7cf9b0bc196bdc14e227a8ae3bb10bf
lisaaa48 <lisasilisasil@gmail.com>
2018-09-12 20:43:01 +0900

    finish frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/cart" => "sale#cart"
+  get "/buy/selectaddress" => "sale#selectaddress"
+  get "/buy/selectpay" => "sale#selectpay"
+  get "/buy/review" => "sale#review"
+  get "/orderplaced" => "sale#orderplaced"
 end
