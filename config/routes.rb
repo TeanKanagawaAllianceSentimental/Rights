cab381cc2b9a17d32e13294620b3165f04c2827e
lisaaa48 <lisasilisasil@gmail.com>
2018-09-15 17:58:55 +0900

    modified routings

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/cart" => "sale#cart"
-  get "/buy/selectaddress" => "sale#selectaddress"
-  get "/buy/selectpay" => "sale#selectpay"
-  get "/buy/review" => "sale#review"
-  get "/orderplaced" => "sale#orderplaced"
-
-  get "/applicant" => "applicants#applicant"
-  get "/application" => "applications#application"
-  get "/selectmusic" => "applications#selectmusic"
-  get "/listofperformedpieces" => "list_of_performed_pieces#listofperformedpieces"
-  get "/application/review" => "applications#review"
-  get "/submitted" => "applications#submitted"
+  resources :applicants do
+    member do
+      get :application1
+    end
+  end
+  resources :applications, only:[:new, :create, :show] do
+    member do
+      get :selectmusic
+      get :review
+      get :submitted
+    end
+  end
+    resources :list_of_performed_pieces, only:[:new, :create, :show]
 end
