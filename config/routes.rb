4aeef495a8ea085d62ca5a4d724ae130b744bfbe
lisaaa48 <lisasilisasil@gmail.com>
2018-09-15 17:50:29 +0900

    modified routings

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/cart" => "sale#cart"
-  get "/buy/selectaddress" => "sale#selectaddress"
-  get "/buy/selectpay" => "sale#selectpay"
-  get "/buy/review" => "sale#review"
-  get "/orderplaced" => "sale#orderplaced"
+  resources :sales, only:[:edit, :update, :create] do
+    member do
+      get :cart
+      get :selectaddress
+      get :selectpay
+      get :review
+      get :orderplaced
+    end
+  end
 end
