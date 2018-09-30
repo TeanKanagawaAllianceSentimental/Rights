4ceedce5e764e52e36d9651f065d5e34f7aeb20c
saboten0807 <will.work.out87@gmail.com>
2018-09-30 12:45:57 +0900

    add front logout path

--- db/schema.rb
+++ db/schema.rb
@@ -45,13 +45,13 @@ ActiveRecord::Schema.define(version: 2018_09_29_031547) do
   end
 
   create_table "carts", force: :cascade do |t|
-    t.integer "member_id"
+    t.string "session_id"
     t.integer "item_id"
     t.integer "quantity"
-    t.integer "unit_price"
-    t.string "session"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "member_id"
+    t.integer "unit_price"
   end
 
   create_table "credit_cards", force: :cascade do |t|
@@ -87,11 +87,11 @@ ActiveRecord::Schema.define(version: 2018_09_29_031547) do
     t.string "artist", null: false
     t.text "jacket_image_id"
     t.string "label", null: false
-    t.integer "unit_price"
+    t.string "unit_price"
     t.string "caption"
     t.text "about"
     t.integer "stock_quantity"
-    t.integer "status", default: 0, null: false
+    t.string "status", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["genre_id"], name: "index_items_on_genre_id"
@@ -154,11 +154,11 @@ ActiveRecord::Schema.define(version: 2018_09_29_031547) do
     t.string "organisation_name"
     t.string "department"
     t.string "contact_person"
-    t.integer "sale_id", default: 0
+    t.integer "sale_id"
   end
 
   create_table "sales", force: :cascade do |t|
-    t.integer "total_price", default: 0
+    t.integer "total_price"
     t.integer "member_id"
     t.integer "delivered"
     t.datetime "delivered_at"
