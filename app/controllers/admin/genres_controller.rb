971297df4c3f91a637b8fc42c98b2f30e78241cd
Ryoji7k <xxx.krkr17@gmail.com>
2018-10-02 05:12:23 +0000

    商品編集機能修正

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -18,13 +18,12 @@ class Admin::GenresController < Admin::AdminBase
 
   def create
   	@genre = Genre.new(genre_params)
-    if @genre.invalid?
-      flash.now[:error] = '入力内容を再度ご確認ください'
-      render :new
-    else
-      @genre.save(genre_params)
+    if @genre.save(genre_params)
       flash[:success] = 'ご登録ありがとうございます'
       redirect_to admin_rights_path
+    else
+      flash.now[:error] = '入力内容を再度ご確認ください'
+      render :new
     end
   end
 
@@ -32,13 +31,12 @@ class Admin::GenresController < Admin::AdminBase
   end
 
   def update
-    if @genre.invalid?
-      flash.now[:error] = '入力内容を再度ご確認ください'
-      render :edit
-    else
-      @genre.update(update_genre_params)
+    if @genre.update(update_genre_params)
       flash[:success] = '編集が完了いたしました'
       redirect_to admin_rights_path
+    else
+      flash.now[:error] = '入力内容を再度ご確認ください'
+      render :edit
     end
   end
 
@@ -59,6 +57,6 @@ class Admin::GenresController < Admin::AdminBase
   end
 
   def update_genre_params
-    params.require(:genre).permit(:id, :genre, items_attributes:[:cd_title, :genre_id, :package, :artist, :jacket_image, :label, :unit_price, :caption, :about, :stock_quantity, :status, :_destroy, :id], disks_attributes:[:disk, :item_id, :_destroy, :id], musics_attributes:[:disk_id, :muscic_title, :songwriter, :composer,:_destroy, :id])
+    params.require(:genre).permit(:id, :genre, items_attributes:[:cd_title, :genre_id, :package, :artist, :jacket_image, :label, :unit_price, :caption, :about, :stock_quantity, :status, :_destroy, :id, disks_attributes:[:disk, :item_id, :_destroy, :id, musics_attributes:[:disk_id, :muscic_title, :songwriter, :composer,:_destroy, :id]]])
   end
 end
